bac4f473d5a14c8bf4e490acc2aadad2548f1cba
Nauraya Dzikra <140572169+NaurayaDzikra@users.noreply.github.com>
2024-05-10 19:47:13 +0700

    Delete order.route.js

--- order.route.js
+++
@@ -1,9 +0,0 @@
-const express = require(`express`)
-const app = express()
-app.use(express.json()) 
-const orderController = require(`../controllers/order.controller`)
-
-app.post('/', orderController.Addorder )
-app.get('/', orderController.getAllHistory)
-
-module.exports=app
